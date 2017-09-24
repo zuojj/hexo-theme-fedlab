aa53b5c8ce97e074a94fbde4985c0a03d9f669f1
zuojj <cuew1987@163.com>
2017-09-24 11:18:43 +0800

    update

--- src/index/index.js
+++ src/index/index.js
@@ -1,1 +1,20 @@
-require('./index.scss')
+require('./index.scss');
+
+var header = document.querySelector('.fedlab-header'),
+    headerH = header.clientHeight;
+
+window.addEventListener('scroll', function() {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if(scrollTop > 0) {
+        header.className.indexOf(' white') < 0 && (header.className += ' white');
+    }else if(scrollTop === 0) {
+        header.className = header.className.replace(/\swhite/, '');
+    }
+
+    if(scrollTop > headerH) {
+        header.className.indexOf(' hide') < 0 && (header.className += ' hide');
+    }else {
+        header.className = header.className.replace(/\shide/, '');
+    }
+})
